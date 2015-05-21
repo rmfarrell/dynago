f922e652c60818c9e3547e78495a66906f654b14
James Crasta <jcrasta@underarmour.com>
2015-05-21 11:56:39 -0600

    Document type marshaling / getting improvements:
     * Add GetStringSet
     * Number can get an int64
     * Don't marshal nil-ary values
     * Document values default to the empty type for their value

--- types.go
+++ types.go
@@ -2,7 +2,9 @@ package dynago
 
 import (
 	"encoding/json"
+	"reflect"
 	"strconv"
+	"time"
 )
 
 type StringSet []string
@@ -15,8 +17,14 @@ type List []interface{}
 
 type Number string
 
+type Time *time.Time
+
 func (n Number) IntVal() (int, error) {
 	return strconv.Atoi(string(n))
+}
+
+func (n Number) Int64Val() (int64, error) {
+	return strconv.ParseInt(string(n), 10, 64)
 }
 
 func (n Number) FloatVal() (float64, error) {
@@ -29,7 +37,9 @@ type Document map[string]interface{}
 func (d Document) MarshalJSON() ([]byte, error) {
 	output := make(map[string]interface{}, len(d))
 	for key, val := range d {
-		output[key] = wireEncode(val)
+		if v := reflect.ValueOf(val); !isEmptyValue(v) {
+			output[key] = wireEncode(val)
+		}
 	}
 	return json.Marshal(output)
 }
@@ -53,11 +63,36 @@ func (d *Document) UnmarshalJSON(buf []byte) error {
 
 // Helper to get a string from a document.
 func (d Document) GetString(key string) string {
-	return d[key].(string)
+	if d[key] != nil {
+		return d[key].(string)
+	} else {
+		return ""
+	}
 }
 
 func (d Document) GetNumber(key string) Number {
-	return d[key].(Number)
+	if d[key] != nil {
+		return d[key].(Number)
+	} else {
+		return Number("")
+	}
+}
+
+func (d Document) GetStringSet(key string) StringSet {
+	if d[key] != nil {
+		return d[key].(StringSet)
+	} else {
+		return StringSet{}
+	}
+}
+
+func (d Document) GetTime(key string) Time {
+	iso8601 := d.GetString(key)
+	t, err := time.ParseInLocation("2006-01-02T15:04:05Z", iso8601, time.UTC)
+	if err != nil {
+		return nil
+	}
+	return Time(&t)
 }
 
 // Helper to build a hash key.
@@ -76,4 +111,16 @@ func HashRangeKey(hashName string, hashVal interface{}, rangeName string, rangeV
 type Param struct {
 	Key   string
 	Value interface{}
+}
+
+func isEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	case reflect.Invalid:
+		return true
+	}
+	return false
 }
---
+++ types_test.go
@@ -0,0 +1,115 @@
+package dynago_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/underarmour/dynago"
+)
+
+func TestNumberIntValReturnsTheValueAsAnInt(t *testing.T) {
+	num := dynago.Number("18")
+	intVal, err := num.IntVal()
+	assert.Equal(t, 18, intVal)
+	assert.Nil(t, err)
+}
+
+func TestNumberIntValReturnsAnErrorIfItCannotParseTheValue(t *testing.T) {
+	num := dynago.Number("nope")
+	intVal, err := num.IntVal()
+	assert.Equal(t, 0, intVal)
+	assert.Error(t, err)
+}
+
+func TestNumberInt64ValReturnsTheValueAsAnInt(t *testing.T) {
+	num := dynago.Number("18")
+	intVal, err := num.Int64Val()
+	assert.Equal(t, int64(18), intVal)
+	assert.Nil(t, err)
+}
+
+func TestNumberInt64ValReturnsAnErrorIfItCannotParseTheValue(t *testing.T) {
+	num := dynago.Number("nope")
+	intVal, err := num.Int64Val()
+	assert.Equal(t, int64(0), intVal)
+	assert.Error(t, err)
+}
+
+func TestNumberFloatValReturnsTheValueAsAnfloat(t *testing.T) {
+	num := dynago.Number("18.12")
+	floatVal, err := num.FloatVal()
+	assert.Equal(t, float64(18.12), floatVal)
+	assert.Nil(t, err)
+}
+
+func TestNumberFloatValReturnsAnErrorIfItCannotParseTheValue(t *testing.T) {
+	num := dynago.Number("nope")
+	floatVal, err := num.FloatVal()
+	assert.Equal(t, float64(0), floatVal)
+	assert.Error(t, err)
+}
+
+func TestDocumentGetStringReturnsTheUnderlyingValueAsAString(t *testing.T) {
+	doc := dynago.Document{"name": "Timmy Testerson"}
+	assert.Equal(t, "Timmy Testerson", doc.GetString("name"))
+}
+
+func TestDocumentGetStringReturnsAnEmptyStringWhenTheKeyIsNotPresent(t *testing.T) {
+	doc := dynago.Document{}
+	assert.Equal(t, "", doc.GetString("name"))
+}
+
+func TestDocumentGetNumberReturnsTheDynagoNumberWrappingTheValue(t *testing.T) {
+	doc := dynago.Document{"id": dynago.Number("12")}
+	assert.Equal(t, dynago.Number("12"), doc.GetNumber("id"))
+}
+
+func TestDocumentGetNumberReturnsAnEmptyNumberWhenTheKeyIsNotPresent(t *testing.T) {
+	doc := dynago.Document{}
+	assert.Equal(t, dynago.Number(""), doc.GetNumber("id"))
+}
+
+func TestDocumentGetNumberPanicsIfTheUnderlyingTypeIsNotANumber(t *testing.T) {
+	doc := dynago.Document{"id": "not-a-dynago-number"}
+	assert.Panics(t, func() {
+		doc.GetNumber("id")
+	})
+}
+
+func TestDocumentGetStringSetReturnsTheStringSetValue(t *testing.T) {
+	doc := dynago.Document{"vals": dynago.StringSet{"val1", "val2"}}
+	assert.Equal(t, dynago.StringSet{"val1", "val2"}, doc.GetStringSet("vals"))
+}
+
+func TestDocumentGetStringSetReturnsAnEmptyStringSetWhenTheKeyDoesNotExist(t *testing.T) {
+	doc := dynago.Document{}
+	assert.Equal(t, dynago.StringSet{}, doc.GetStringSet("vals"))
+}
+
+func TestDocumentGetStringSetPanic(t *testing.T) {
+	doc := dynago.Document{"vals": "not-a-string-slice"}
+	assert.Panics(t, func() {
+		doc.GetStringSet("vals")
+	})
+}
+
+func TestDocumentGetTimeReturnsTheTimeValueFromISO8601(t *testing.T) {
+	doc := dynago.Document{"time": "1990-04-16T00:00:00Z"}
+	val, _ := time.Parse("2006-01-02T15:04:05Z", "1990-04-16T00:00:00Z")
+	assert.Equal(t, dynago.Time(&val), doc.GetTime("time"))
+}
+
+func TestDocumentGetTimeReturnsNilWhenTheKeyDoesNotExist(t *testing.T) {
+	doc := dynago.Document{}
+	assert.Nil(t, doc.GetTime("time"))
+}
+
+func TestDocumentMarshalJSONDoesNotIncludeEmptyValues(t *testing.T) {
+	doc := dynago.Document{"key1": "shows up", "key2": 9, "fields": dynago.StringSet([]string{"is", "present"}), "id": "", "name": nil, "tags": []string{}}
+	jsonDoc, _ := doc.MarshalJSON()
+
+	assert.Contains(t, string(jsonDoc), `"fields":{"SS":["is","present"]}`)
+	assert.Contains(t, string(jsonDoc), `"key1":{"S":"shows up"}`)
+	assert.Contains(t, string(jsonDoc), `"key2":{"N":"9"}`)
+}
